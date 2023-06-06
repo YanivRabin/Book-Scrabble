28ff5bd0da5c667ec942a2ba787d8e1a9d15ed38
JoniMalki <Joni910.malki@gmail.com>
2023-06-06 23:34:56 +0300

    Init JsonHandler, MessageHandler,Test JsonTest change message to jsons , not creating test foe sending json between users

--- src/main/java/model/logic/MessageHandler.java
+++ src/main/java/model/logic/MessageHandler.java
@@ -1,134 +1,23 @@
 package model.logic;
 
 import com.google.gson.JsonObject;
-import model.data.Tile;
-
-import java.net.Socket;
 
 public class MessageHandler {
 
     public JsonHandler jsonHandler;
 
-    /**
-     * The MessageHandler function is responsible for handling the messages that are sent to the server.
-     * It takes in a message and then parses it into a JSONObject, which is then used to determine what type of message was sent.
-     * Depending on what type of message was received, different actions will be taken by the MessageHandler function.
-
-     *
-     *
-     * @return A jsonhandler object
-     *
-     * @docauthor Trelent
-     */
     public MessageHandler() {
         jsonHandler = new JsonHandler();
     }
 
-    /**
-     * The CreateStartGameMessage function creates a JSON message that is sent to all clients when the game starts.
-     *
-     *
-     * @param String tilesInCapital Add the tiles to the message
-     * @param String hostNickName Identify the host of the game
-     *
-     * @return A jsonobject
-     *
-     * @docauthor Trelent
-     */
-    public void CreateStartGameMessage(String tilesInCapital, String hostNickName, int playerIndex, int numOfPlayers){
+    public void CreateStartGameMessage(String tilesInCapital){
         // only serverHost
         this.jsonHandler.addMessageType("start game");
-        this.jsonHandler.addSource(hostNickName);
+        this.jsonHandler.addSource("Host");
         this.jsonHandler.addStartTiles(tilesInCapital);
-        this.jsonHandler.addPlayerIndex(playerIndex);
-        this.jsonHandler.addNumOfPlayers(numOfPlayers);
     }
-    /**
-     * The CreateTryAgainMessage function creates a message that is sent to the client when they have lost.
-     * The message contains information about their previous score, and the action that was performed by the serverHost.
-     *
-     *
-     * @param String destination Specify the destination of the message
-     * @param int prevScore Send the previous score to the client so that it can be displayed in a message
-     * @param String action Determine if the player is trying to connect or disconnect
-     * @param String hostNickName Identify the host of the game
-     *
-     * @return A jsonobject
-     *
-     * @docauthor Trelent
-     */
-    public void CreateTryAgainMessage(String destination, int prevScore, String action, String hostNickName){
-        // only serverHost
-        this.jsonHandler.addMessageType("try again");
-        this.jsonHandler.addAction(action);
-        this.jsonHandler.addSource(hostNickName);
-        this.jsonHandler.addDestination(destination);
-        this.jsonHandler.addNewScore(prevScore);
-        // this.jsonHandler.addBoard(board);
-        // addPrevBoard
-    }
-    /**
-     * The CreateSuccessMessage function is used to create a success message.
-     *
-     *
-     * @param String destination Specify the destination of the message
-     * @param int newScore Add the new score to the json message
-     * @param String action Determine what type of message is being sent
-     * @param String newCurrentTiles Add the new tiles to the currenttiles field in the json message
-     * @param String hostNickName Identify the host of the game
-     *
-     * @return A jsonobject
-     *
-     * @docauthor Trelent
-     */
-    public void CreateSuccessMessage(String destination, int newScore, String action, String newCurrentTiles, String hostNickName){
-        // only serverHost
-        this.jsonHandler.addMessageType("success");
-        this.jsonHandler.addAction(action);
-        this.jsonHandler.addSource(hostNickName);
-        this.jsonHandler.addDestination(destination);
-        this.jsonHandler.addNewScore(newScore);
-        this.jsonHandler.addNewCurrentTiles(newCurrentTiles);
-        // this.jsonHandler.addBoard(board);
-        // addPrevBoard
-    }
-    /**
-     * The CreateSucceededChallengeYouMessage function creates a message that is sent to the client who has been challenged by another player.
-     *
-     *
-     * @param Character[][] board Send the board to the client
-     * @param String hostNickName Identify the host of the game
-     *
-     * @return A jsonobject
-     *
-     * @docauthor Trelent
-     */
-    public void CreateSucceededChallengeYouMessage(String hostNickName, String prevScore){
-        // only serverHost
-        this.jsonHandler.addMessageType("succeeded in challenging you");
-        this.jsonHandler.addSource(hostNickName);
-        this.jsonHandler.addPrevScore(Integer.parseInt(prevScore));
-    }
-    /**
-     * The CreateUpdateBoardMessage function creates a message that updates the board.
-     *
-     *
-     * @param  board Send the board to the client
-     * @param  hostNickName Identify the source of the message
-     *
-     * @return A jsonobject
-     *
-     * @docauthor Trelent
-     */
-    public void CreateUpdateBoardMessage(String board, String hostNickName){
-        // only serverHost
-        this.jsonHandler.addMessageType("update board");
-        this.jsonHandler.addSource(hostNickName);
-        this.jsonHandler.addBoard(board);
-    }
-
-    public void CreateTryPlaceWordMessage(String source, String destination, String word, int prevScore,
-                                          int row, int column, boolean vertical, String currentTiles, String socketSource){
+    public void CreateTryPlaceWordMessage(String source, String destination, String word,
+                                          int row, int column, boolean vertical, String currentTiles){
         this.jsonHandler.addMessageType("try place word");
         this.jsonHandler.addSource(source);
         this.jsonHandler.addDestination(destination);
@@ -137,34 +26,11 @@ public class MessageHandler {
         this.jsonHandler.addColumn(column);
         this.jsonHandler.addVertical(vertical);
         this.jsonHandler.addCurrentTiles(currentTiles);
-        this.jsonHandler.addSocketSource(socketSource);
-        this.jsonHandler.addPrevScore(prevScore);
 //        this.jsonHandler.addBoard(board);
         // addPrevBoard
     }
-    /**
-     * The CreateChallengeMessage function creates a JSON message that is sent to the server
-     * when a player challenges another player's word. The function takes in the source, destination,
-     * and word of the challenge as well as where on the board it was placed (row and column) and whether or not it was placed vertically.
-
-     *
-     * @param String source Identify the player who sent the message
-     * @param String destination Specify the destination of the message
-     * @param String word Add the word to the message
-     * @param int row Indicate the row of the first letter in a word
-     * @param int column Specify the column in which the word is placed
-    public void createchallengeresponsemessage(string source, string destination, boolean valid){
-            this
-     * @param boolean vertical Indicate whether the word is placed vertically or horizontally
-     * @param String currentTiles Add the current tiles to the json message
-
-     *
-     * @return A jsonobject with the following fields:
-     *
-     * @docauthor Trelent
-     */
     public void CreateChallengeMessage(String source, String destination, String word,
-                                       int row, int column, boolean vertical, String currentTiles, String socketSource){
+                                       int row, int column, boolean vertical, String currentTiles){
         this.jsonHandler.addMessageType("challenge");
         this.jsonHandler.addSource(source);
         this.jsonHandler.addDestination(destination);
@@ -173,19 +39,41 @@ public class MessageHandler {
         this.jsonHandler.addColumn(column);
         this.jsonHandler.addVertical(vertical);
         this.jsonHandler.addCurrentTiles(currentTiles);
-        this.jsonHandler.addSocketSource(socketSource);
+        // this.jsonHandler.addBoard(board);
+        // addPrevBoard
     }
-
-    public void CreateMessageToGameServer(String message, String socketSource){
-        this.jsonHandler.addMessageType("to game server");
-        this.jsonHandler.addMessage(message);
-        this.jsonHandler.addSocketSource(socketSource);
+    public void CreateTryAgainMessage(String destination, int prevScore, String action){
+        // only serverHost
+        this.jsonHandler.addMessageType("try again");
+        this.jsonHandler.addAction(action);
+        this.jsonHandler.addSource("Host");
+        this.jsonHandler.addDestination(destination);
+        this.jsonHandler.addNewScore(prevScore);
+        // this.jsonHandler.addBoard(board);
+        // addPrevBoard
     }
-
-    public void createPassTurnMessage() {
-
-        this.jsonHandler.addMessageType("pass turn");
+    public void CreateSuccessMessage(String destination, int newScore, String action, String newCurrentTiles){
+        // only serverHost
+        this.jsonHandler.addMessageType("success");
+        this.jsonHandler.addAction(action);
+        this.jsonHandler.addSource("Host");
+        this.jsonHandler.addDestination(destination);
+        this.jsonHandler.addNewScore(newScore);
+        this.jsonHandler.addNewCurrentTiles(newCurrentTiles);
+        // this.jsonHandler.addBoard(board);
+        // addPrevBoard
     }
-
+    public void CreateSucceededChallengeYouMessage(Character[][] board){
+        // only serverHost
+        this.jsonHandler.addMessageType("succeeded in challenging you");
+        this.jsonHandler.addSource("Host");
+        this.jsonHandler.addBoard(board);
+    }
+    public void CreateUpdateBoardMessage(Character[][] board){
+        // only serverHost
+        this.jsonHandler.addMessageType("update board");
+        this.jsonHandler.addSource("Host");
+        this.jsonHandler.addBoard(board);
+    }
 
 }
