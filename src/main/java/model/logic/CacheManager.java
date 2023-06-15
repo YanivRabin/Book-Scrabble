23f6fcf658267116083c9700877d3a172296158e
JoniMalki <Joni910.malki@gmail.com>
2023-06-15 21:50:53 +0300

    change to blocking queue , test try place success !

--- src/main/java/model/logic/CacheManager.java
+++ src/main/java/model/logic/CacheManager.java
@@ -10,6 +10,19 @@ public class CacheManager {
     CacheReplacementPolicy crp;
     HashSet<String> cache;
 
+    /**
+     * The CacheManager function is responsible for managing the cache.
+     * It will add a new page to the cache if it does not already exist in the cache, and update its frequency.
+     * If it does exist, then we simply update its frequency.
+
+     *
+     * @param int s Set the size of the cache
+     * @param CacheReplacementPolicy c Determine which cache replacement policy to use
+     *
+     * @return A new cachemanager object with the given size and replacement policy
+     *
+     * @docauthor Trelent
+     */
     public CacheManager(int s, CacheReplacementPolicy c) {
 
         size = s;
@@ -17,8 +30,31 @@ public class CacheManager {
         cache = new HashSet<>();
     }
 
+    /**
+     * The query function takes a string as input and returns true if the string is in the cache,
+     * false otherwise.
+
+     *
+     * @param String str Check if the cache contains a specific string
+     *
+     * @return True if and only if for some k &gt;= 1, the last k characters queried (in order from oldest to newest, including this letter just queried) spell one of the strings in the given list
+     *
+     * @docauthor Trelent
+     */
     public boolean query(String str) { return cache.contains(str); }
 
+    /**
+     * The add function adds a string to the cache.
+     * If the cache is full, it removes an element from the cache using crp's remove function.
+     * Then, it adds str to both crp and cache.
+
+     *
+     * @param String str Add a string to the cache
+     *
+     * @return Void
+     *
+     * @docauthor Trelent
+     */
     public void add(String str) {
 
         if (cache.size() >= size)
--- src/main/java/model/logic/IOSearcher.java
+++ src/main/java/model/logic/IOSearcher.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 
 public class IOSearcher {
 
+    /**
+     * The search function takes a string and an array of strings as arguments.
+     * It then searches through each file in the array for the given string,
+     * returning true if it is found and false otherwise.
+
+     *
+     * @param String str Search for a word in the files
+     * @param String... args Pass in an array of strings
+     *
+     * @return True if the string is found in any of the files
+     *
+     * @docauthor Trelent
+     */
     public static boolean search(String str, String... args)  {
 
         String[] files = args;
