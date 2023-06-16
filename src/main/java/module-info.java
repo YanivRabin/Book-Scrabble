a0a0b96030d786080d822f94c5c97efa10a71086
YanivRabin <rabin.yaniv@gmail.com>
2023-06-16 18:23:59 +0300

    update gui, making tiles and function for them

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module view.bookscrabble {
     requires javafx.controls;
     requires javafx.fxml;
     requires junit;
-    requires com.jfoenix;
+//    requires com.jfoenix;
+
 
 
     opens view to javafx.fxml;
