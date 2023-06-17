999f3d391fb9675b61c4a9ab64d7b3877e86d5c9
YanivRabin <rabin.yaniv@gmail.com>
2023-06-17 17:39:05 +0300

    fix tryPlaceWord and adding books for test

--- src/main/java/model/logic/IOSearcher.java
+++ src/main/java/model/logic/IOSearcher.java
@@ -1,23 +1,11 @@
 package model.logic;
 import java.io.*;
+import java.util.Locale;
 import java.util.Scanner;
 
 
 public class IOSearcher {
 
-    /**
-     * The search function takes a string and an array of strings as arguments.
-     * It then searches through each file in the array for the given string,
-     * returning true if it is found and false otherwise.
-
-     *
-     * @param String str Search for a word in the files
-     * @param String... args Pass in an array of strings
-     *
-     * @return True if the string is found in any of the files
-     *
-     * @docauthor Trelent
-     */
     public static boolean search(String str, String... args)  {
 
         String[] files = args;
@@ -31,12 +19,13 @@ public class IOSearcher {
                 while (scanner.hasNext()) {
 
                     arrWords = scanner.next().split(" ");
-                    for (String word : arrWords){
-                        String w = word.toUpperCase();
-                        if(w.equals(str)){
+                    for (String word : arrWords) {
+
+                        String word2 = word.toUpperCase();
+                        if (word2.equals(str))
                             return true;
-                        }
                     }
+
                 }
                 scanner.close();
             }
