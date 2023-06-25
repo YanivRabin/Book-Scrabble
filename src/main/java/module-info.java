aa1fc8c6df442fffa45d83bb62aa552cb0327560
JoniMalki <Joni910.malki@gmail.com>
2023-06-25 13:47:07 +0300

    create connection between multiply computers

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,11 @@ module view.bookscrabble {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
-//    requires junit;
+    requires junit;
 
     opens view to javafx.fxml;
     exports view;
+    exports viewModel;
+    exports model.data;
+    exports model.logic;
 }
